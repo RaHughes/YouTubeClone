9a6a879f799a54f0b81e17f64e9f7bf536653dd3
RaHughes <rogerallenhughes@hotmail.com>
2021-11-29 10:39:49 -0600

    changed logic flow for comments functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,8 +10,6 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            comments: [],
-            replies: [],
             videoId: '',
             video: '',
             videos: []
