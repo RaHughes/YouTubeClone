d63ed0d89d5f39d19f25ca5f8242d15b82a85e5b
James Corbett <wjamescorbett@gmail.com>
2021-11-30 15:43:54 -0500

    Video sidebar now has the select video button above the video.

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'; 
 import axios from 'axios';
+import './SideBar.css'
 
 const SideBar = (props) => {
 
@@ -11,12 +12,12 @@ const SideBar = (props) => {
         )
     }
     else{
-        return(<div className="videos">
+        return(<div>
             {props.videos.map(video => {
-                return <form key={Math.random()} onSubmit={() => props.selectVideo(video.id.videoId, video)}> 
+                return <form className="videos" key={Math.random()} onSubmit={() => props.selectVideo(video.id.videoId, video)}>
+                <button type="submit">Select Video</button>
                 <iframe width="300" height="140" src={`https://www.youtube.com/embed/${video.id.videoId}`}>
                 </iframe>
-                <button type="submit">Select Video</button>
                 </form>
             })}
             </div>
